3a6f5219a9121baacf6e3993e4fc0802aecd2569
skamer <skamer@skamer.com>
2021-04-11 19:07:24 +0200

    Fixed the Github flow

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
 
 export default () => {
   return (
@@ -19,3 +19,5 @@ export default () => {
     </div>
   );
 };
+
+// Comment
